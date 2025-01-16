552e269ba94ae2c4d2e54b5c593527772b4807a3
rumeysa yakut <131006594+morirum@users.noreply.github.com>
2025-01-16 20:26:37 +0300

    Add files via upload

--- index.php
+++ index.php
@@ -1,13 +1,59 @@
 <?php
+
+const title="Popüler Kurslar";
+
+$kategoriler = array("Programlama","Web Gelliştirme","Veri Analizi");
+
+sort($kategoriler);
+
+$kurslar= array(
+    "1"=> array(
+        "baslik" => "Php Kursu",
+        "altBaslik" =>"Sıfırdan ileri seviye Php ile web geliştirme",
+        "resim" =>"php.png",
+        "yayinTarihi"=>"01.01.2023",
+        "yorumSayisi" => "100",
+        "begeniSayisi" => "300"
+    ),
+    "2" => array(
+    "baslik" => "Pthton Kursu",
+    "altBaslik" =>"Sıfırdan ileri seviye Pyhton geliştirme",
+    "resim" =>"pyhton.jpg",
+    "yayinTarihi"=>"03.03.2023",
+    "yorumSayisi" => "200",
+    "begeniSayisi" => "400"
+    ),
+    "3" => array(
+    "baslik" => "Node.js Kursu",
+    "altBaslik" =>"Sıfırdan ileri seviye Node.js ile web geliştirme",
+    "resim" =>"nodejs.jpg",
+    "yayinTarihi"=>"05.05.2023",
+    "yorumSayisi" => "300",
+    "begeniSayisi" => "500"
+    ),
+);
+
+$yeni_kurs = array(
+    "baslik" => "Django Kursu",
+    "altBaslik" =>"Sıfırdan ileri seviye Django programlama",
+    "resim" =>"nodejs.jpg",
+    "yayinTarihi"=>"05.05.2023",
+    "yorumSayisi" => "300",
+    "begeniSayisi" => "500"
+);
+
+$kurslar["4"]= $yeni_kurs;
+
+
 $kurs1_baslik="Php Kursu";
-$kurs1_altBaslik="Sıfırdan ileri seviye php ile web geliştirme";
+$kurs1_altBaslik="Sıfırdan ileri seviye Php ile web geliştirme";
 $kurs1_resim ="php.png";
 $kurs1_yayinTarihi = "01.01.2023";
 $kurs1_yorumSayisi = "100";
 $kurs1_begeniSayisi = "300";
 
 $kurs2_baslik="Pyhton Kursu";
-$kurs2_altBaslik="Sıfırdan ileri seviye pyhton geliştirme";
+$kurs2_altBaslik="Sıfırdan ileri seviye Pyhton geliştirme";
 $kurs2_resim ="pyhton.jpg";
 $kurs2_yayinTarihi = "03.03.2023";
 $kurs2_yorumSayisi = "200";
@@ -20,6 +66,18 @@ $kurs3_yayinTarihi = "05.05.2023";
 $kurs3_yorumSayisi = "300";
 $kurs3_begeniSayisi = "500";
 
+$kurs1_altBaslik = ucfirst(strtolower($kurslar["1"]["altBaslik"]));
+$kurs2_altBaslik = ucfirst(strtolower($kurslar["1"]["altBaslik"]));
+$kurs3_altBaslik = ucfirst(strtolower($kurslar["1"]["altBaslik"]));
+
+$kurs1_altBaslik = substr($kurs1_altBaslik,0,50)."...";
+$kurs2_altBaslik = substr($kurs2_altBaslik,0,50)."...";
+$kurs3_altBaslik = substr($kurs3_altBaslik,0,50)."...";
+
+
+$kurs1_url = str_replace([" ","I"],["-","i"],strtolower($kurslar["1"]["baslik"]));
+$kurs2_url = str_replace([" ","ç"],["-","c"],strtolower($kurslar["2"]["baslik"]));
+$kurs2_url = str_replace([" ","ö"],["-","o"],strtolower($kurslar["3"]["baslik"]));
 
 ?>
 
@@ -33,24 +91,42 @@ $kurs3_begeniSayisi = "500";
 </head>
 <body>
    <div class="container my-3">
+    <div class="row">
+        <div class="col-3">
+            <a href="#" class="list-group-item list-group-item-action"><?php echo $kategoriler[0];?></a>
+            <a href="#" class="list-group-item list-group-item-action"><?php echo $kategoriler[1];?></a>
+            <a href="#" class="list-group-item list-group-item-action"><?php echo $kategoriler[2];?></a>
+
+
+
+        </div>
+        <div class="col-9">
+            <h1 class="mb-3"><?php echo title; ?></h1>
+            <p class="lead">
+                <?php echo count($kategoriler)?> kategoride <?php echo count($kurslar)?> kurs listelenmiştir.
+            </p>
         <div class="card mb-3">
             <div class="row">
                 <div class="col-3">
-                    <img src="img/<?php echo $kurs1_resim; ?>" alt="" class="img-fluid rounded-start">
+                    <img src="img/<?php echo $kurslar["1"]["resim"]; ?>" alt="" class="img-fluid rounded-start">
                 </div>
-                <div class="col-9">
+                <div class="col-8">
                     <div class="card-body">
-                        <h5 class="card-title"><?php echo $kurs1_baslik; ?></h5>
-                        <p class="card-text"><?php echo $kurs1_altBaslik; ?></p>
+                        <h5 class="card-title">
+                            <a href="<?php echo $kurs1_url; ?>"> 
+                            <?php echo $kurslar["1"]["baslik"]; ?>
+                            </a>
+                        </h5>
+                        <p class="card-text"><?php echo ucfirst(strtolower($kurslar["1"]["altBaslik"])); ?></p>
                         <p>
                             <span class="badge rounded-pill text-bg-primary">
-                                Beğeni: <?php echo $kurs1_begeniSayisi; ?>
+                                Beğeni: <?php echo $kurslar["1"]["begeniSayisi"]; ?>
                             </span>
                         </p>
 
                             <p>
                             <span class="badge rounded-pill text-bg-danger">
-                                Yorum Sayısı: <?php echo $kurs1_yorumSayisi; ?>
+                                Yorum Sayısı: <?php echo $kurslar["1"]["yorumSayisi"]; ?>
                             </span>
                         </p>                    
                     </div>
@@ -61,21 +137,54 @@ $kurs3_begeniSayisi = "500";
         <div class="card mb-3">
             <div class="row">
                 <div class="col-3">
-                    <img src="img/<?php echo $kurs2_resim; ?>" alt="" class="img-fluid rounded-start">
+                    <img src="img/<?php echo $kurslar["2"]["resim"]; ?>" alt="" class="img-fluid rounded-start">
                 </div>
-                <div class="col-9">
+                <div class="col-8">
                     <div class="card-body">
-                        <h5 class="card-title"><?php echo $kurs2_baslik; ?></h5>
-                        <p class="card-text"><?php echo $kurs2_altBaslik; ?></p>
+                        <h5 class="card-title">
+                            <a href="<?php echo $kurs1_url; ?>"> 
+                            <?php echo $kurslar["2"]["baslik"]; ?>
+                            </a>
+                        </h5>
+                        <p class="card-text"><?php echo ucfirst(strtolower($kurslar["2"]["altBaslik"])); ?></p>
                         <p>
                             <span class="badge rounded-pill text-bg-primary">
-                                Beğeni: <?php echo $kurs2_begeniSayisi; ?>
+                                Beğeni: <?php echo $kurslar["2"]["begeniSayisi"]; ?>
                             </span>
                         </p>
 
                             <p>
                             <span class="badge rounded-pill text-bg-danger">
-                                Yorum Sayısı: <?php echo $kurs2_yorumSayisi; ?>
+                                Yorum Sayısı: <?php echo $kurslar["2"]["yorumSayisi"]; ?>
+                            </span>
+                        </p>                    
+                    </div>
+                </div>
+            </div>
+
+        </div>        
+        <div class="card mb-3">
+            <div class="row">
+                <div class="col-3">
+                    <img src="img/<?php echo $kurslar["3"]["resim"]; ?>" alt="" class="img-fluid rounded-start">
+                </div>
+                <div class="col-8">
+                    <div class="card-body">
+                        <h5 class="card-title">
+                            <a href="<?php echo $kurs1_url; ?>"> 
+                            <?php echo $kurslar["3"]["baslik"]; ?>
+                            </a>
+                        </h5>
+                        <p class="card-text"><?php echo ucfirst(strtolower($kurslar["3"]["altBaslik"])); ?></p>
+                        <p>
+                            <span class="badge rounded-pill text-bg-primary">
+                                Beğeni: <?php echo $kurslar["3"]["begeniSayisi"]; ?>
+                            </span>
+                        </p>
+
+                            <p>
+                            <span class="badge rounded-pill text-bg-danger">
+                                Yorum Sayısı: <?php echo $kurslar["3"]["yorumSayisi"]; ?>
                             </span>
                         </p>                    
                     </div>
@@ -86,21 +195,25 @@ $kurs3_begeniSayisi = "500";
         <div class="card mb-3">
             <div class="row">
                 <div class="col-3">
-                    <img src="img/<?php echo $kurs3_resim; ?>" alt="" class="img-fluid rounded-start">
+                    <img src="img/<?php echo $kurslar["4"]["resim"]; ?>" alt="" class="img-fluid rounded-start">
                 </div>
-                <div class="col-9">
+                <div class="col-8">
                     <div class="card-body">
-                        <h5 class="card-title"><?php echo $kurs3_baslik; ?></h5>
-                        <p class="card-text"><?php echo $kurs3_altBaslik; ?></p>
+                        <h5 class="card-title">
+                            <a href="<?php echo $kurs1_url; ?>"> 
+                            <?php echo $kurslar["4"]["baslik"]; ?>
+                            </a>
+                        </h5>
+                        <p class="card-text"><?php echo ucfirst(strtolower($kurslar["4"]["altBaslik"])); ?></p>
                         <p>
                             <span class="badge rounded-pill text-bg-primary">
-                                Beğeni: <?php echo $kurs3_begeniSayisi; ?>
+                                Beğeni: <?php echo $kurslar["4"]["begeniSayisi"]; ?>
                             </span>
                         </p>
 
                             <p>
                             <span class="badge rounded-pill text-bg-danger">
-                                Yorum Sayısı: <?php echo $kurs3_yorumSayisi; ?>
+                                Yorum Sayısı: <?php echo $kurslar["4"]["yorumSayisi"]; ?>
                             </span>
                         </p>                    
                     </div>
@@ -108,8 +221,9 @@ $kurs3_begeniSayisi = "500";
             </div>
 
         </div>
+
             
-        </div>
+    </div>
         
    </div>
 </body>
